930301444b52fd1f0b79f0cd540256cffb0a63ca
mehrshadevilb-cell <mehrshad.evil.b@gmail.com>
2026-09-15 16:03:14 +0330

    feat: add AI music advanced workflow states

--- src/bot/states/music_states.py
+++ src/bot/states/music_states.py
@@ -4,4 +4,7 @@ from aiogram.fsm.state import State, StatesGroup
 class MusicState(StatesGroup):
     waiting_prompt = State()
     choosing_output = State()
+    advanced = State()
+    waiting_edit_prompt = State()
+    waiting_extend = State()
     generating = State()
