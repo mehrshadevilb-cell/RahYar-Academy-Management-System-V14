65952c11f61ae98c5eb12e8644a10c8034c836a9
mehrshadevilb-cell <mehrshad.evil.b@gmail.com>
2026-09-15 15:55:41 +0330

    Replace rigid music flow with AI prompt generation states

--- src/bot/states/music_states.py
+++ src/bot/states/music_states.py
@@ -2,6 +2,6 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class MusicState(StatesGroup):
-    waiting_key = State()
-    waiting_progression = State()
-    choosing_instrument = State()
+    waiting_prompt = State()
+    choosing_output = State()
+    generating = State()
