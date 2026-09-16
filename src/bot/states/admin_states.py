1c23daedd4cf8eb837aca3b8285c1896176b4e9f
mehrshadevilb-cell <mehrshad.evil.b@gmail.com>
2026-09-16 14:04:12 +0330

    feat: wire class chat into admin menu, states, bot, tests

--- src/bot/states/admin_states.py
+++ src/bot/states/admin_states.py
@@ -8,13 +8,11 @@ class AdminState(StatesGroup):
     waiting_new_card_holder = State()
     waiting_student_phone = State()
 
-    # --- Product integrations ---
     waiting_spotplayer_course_id = State()
     waiting_spotplayer_course_name = State()
     waiting_channel_name = State()
     waiting_channel_chat_id = State()
 
-    # --- Online course CRUD ---
     waiting_online_course_name = State()
     waiting_online_course_teacher = State()
     waiting_online_course_duration = State()
@@ -25,25 +23,21 @@ class AdminState(StatesGroup):
     waiting_online_course_field_edit = State()
     waiting_online_slot = State()
 
-    # --- Online enrollment management ---
     waiting_enrollment_sessions = State()
     waiting_enrollment_fee = State()
     waiting_enrollment_note = State()
+    waiting_class_chat = State()
 
-    # --- Discount codes ---
     waiting_discount_code_text = State()
     waiting_discount_code_type = State()
     waiting_discount_code_value = State()
     waiting_discount_code_max_uses = State()
     waiting_discount_code_expiry = State()
 
-    # --- Admin logs ---
     waiting_log_search_keyword = State()
 
-    # --- Broadcast ---
     waiting_broadcast_content = State()
     waiting_broadcast_confirm = State()
 
-    # --- AI Developer Agent ---
     waiting_ai_task = State()
     waiting_ai_consult = State()
